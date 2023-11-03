c0e4c206c1581a86096608e5137dbd0774e196bb
Mark Bennison <markbennison@users.noreply.github.com>
2023-11-03 17:14:42 +0000

    Updated to include lockout

--- Assets/Scripts/NumberSelector/CodeChecker.cs
+++ Assets/Scripts/NumberSelector/CodeChecker.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,11 +8,40 @@ public class CodeChecker : MonoBehaviour
 {
     [SerializeField]
     MultipleDigitSelector multipleDigitSelector;
+
+    [SerializeField]
+    GameObject lockoutText;
+
     string PassCode = "6D1";
+
+    bool lockout = false;
+    float lockoutTimer = 0;
+
+    private void Start()
+    {
+        lockoutText.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if(lockoutTimer > 0)
+        {
+            lockoutTimer -= Time.deltaTime;
+            if (lockoutTimer <= 0)
+            {
+                lockoutTimer = 0;
+                lockoutText.SetActive(false);
+                Debug.Log("Active FALSE");
+            }
+
+            Debug.Log(lockoutTimer);
+            SetTimerTextUI();
+        }
+    }
 
     public void ConfirmButtonClick()
     {
-        if(multipleDigitSelector != null)
+        if(multipleDigitSelector != null && lockoutTimer <= 0)
         {
             if(string.Compare(multipleDigitSelector.GetHexValue(),PassCode) == 0)
             {
@@ -21,7 +51,18 @@ public class CodeChecker : MonoBehaviour
             else
             {
                 Debug.Log("INCORRECT CODE ("+ multipleDigitSelector.GetHexValue() +" != " + PassCode + ")");
+                lockoutTimer = 20f;
+                SetTimerTextUI();
+                lockoutText.SetActive(true);
+                Debug.Log("Active TRUE");
             }
         }
     }
+
+    private void SetTimerTextUI()
+    {
+        lockoutText.GetComponent<TextMeshProUGUI>().text = "LOCKED OUT..." + Mathf.Floor(lockoutTimer).ToString();
+    }
+
+
 }
